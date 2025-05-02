e568e26fb6a728faf8928101fd3fb2527faf20ad
Dimooon4220 <dimaburavlov@gmail.com>
2025-05-02 16:20:34 +0300

    Add files via upload

---
+++ Java.js
@@ -0,0 +1,4 @@
+// Simple example: Alert when the page is loaded
+document.addEventListener("DOMContentLoaded", function() {
+    alert("Ласкаво просимо на сайт Спортивного клубу Маяк Вінниця!");
+});
